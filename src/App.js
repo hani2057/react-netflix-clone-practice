8baeed07eb45587084690679ea1fe8a9314aaa1d
hani2057 <hanikim2057@gmail.com>
2023-01-06 21:32:03 +0900

    Apply react-router-dom to route pages

--- src/App.js
+++ src/App.js
@@ -1,49 +1,33 @@
-import requests from "./api/requests";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Banner from "./components/Banner";
 import Nav from "./components/Nav";
-import Row from "./components/Row";
 import Footer from "./components/Footer";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchURL={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" id="TN" fetchURL={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchURL={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchURL={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchURL={requests.fetchComedyMovies}
-      />
-      <Row
-        title="Horror Movies"
-        id="HM"
-        fetchURL={requests.fetchHorrorMovies}
-      />
-      <Row
-        title="Romance Movies"
-        id="RM"
-        fetchURL={requests.fetchRomanceMovies}
-      />
-      <Row
-        title="Documentaries"
-        id="DC"
-        fetchURL={requests.fetchDocumentaries}
-      />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
